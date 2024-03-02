6b1e84debc6836840e6e387c62de885ead2e8506
Mahdi <mehdirez1i@gmail.com>
2024-03-02 17:58:45 +0330

    just remove password

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,5 @@
 <?php 
-$connection = mysqli_connect('localhost', 'mahdi', '159875321MM','test_db');
+$connection = mysqli_connect('localhost', 'mahdi', '<PASSWORD>','test_db');
 if(mysqli_connect_errno()){
     echo 'connention faild'. mysqli_connect_error();
 }
