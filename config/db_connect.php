0d6d994130a0ffcf33fed3c1872e361610eaa46f
Mahdi <mehdirez1i@gmail.com>
2024-03-03 00:05:35 +0330

    add access token

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,5 @@
 <?php 
-include './db_info.php';
+include 'db_info.php';
 $connection = mysqli_connect($host, $username, $password, $db_name);
 if(mysqli_connect_errno()){
     echo 'connention faild'. mysqli_connect_error();
--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $request_method = $_SERVER["REQUEST_METHOD"];
 
 switch ($request_method) {
 	case 'GET':
-		echo 'user not find';
+		echo 'you not access to this oprtion';
 		break;
 	case 'POST':
 		$data = json_decode(file_get_contents('php://input'), true);
@@ -24,7 +24,7 @@ function loginUser($email, $password)
 	$result = $connection->query($sql);
 	$row = $result->fetch_assoc(); // fetch_all();
 	if ($result->num_rows > 0) {
-		
+
 		$token = getToken($row);
 		header("HTTP/1.0 403");
 		$response = array(
@@ -43,8 +43,22 @@ function loginUser($email, $password)
 	echo json_encode($response);
 }
 
-function getToken($id){
+function getToken($row)
+{
+	global $connection;
+	$t = time();
 	$token = bin2hex(random_bytes(16));
 	//$row['id'];
+	$sql = "SELECT * FROM u_token WHERE user_id= '" . $row['id'] . "' ";
+	$result = $connection->query($sql);
+	$user = $result->fetch_assoc(); // fetch_all();
+	if ($result->num_rows == 0) {
+		$sql2 = "INSERT INTO u_token ( user_id, login_time, access_token) VALUES ('" . $row['id'] . "','" . round(microtime(true) * 1000) . "','" . $token . "')";
+		$result = $connection->query($sql2);
+	} else {
+		echo 'something';
+		$sql_update = "UPDATE  u_token SET access_token='" . $token . "', login_time ='" . round(microtime(true) * 1000) . "' WHERE  user_id = '" . $row['id'] . "'  ";
+		$result = $connection->query($sql_update);
+	}
 	return $token;
 }
