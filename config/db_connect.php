0bed8eca6f858a213b77f553351af93b419acd0c
Mahdi <mehdirez1i@gmail.com>
2024-03-02 18:39:32 +0330

    add gitignore file and create db_info.php for connect config

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,6 @@
 <?php 
-$connection = mysqli_connect('localhost', 'mahdi', '<PASSWORD>','test_db');
+include './db_info.php';
+$connection = mysqli_connect($host, $username, $password, $db_name);
 if(mysqli_connect_errno()){
     echo 'connention faild'. mysqli_connect_error();
 }
